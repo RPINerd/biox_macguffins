7ec53d197b724581e9ed7780222b7a4deb0d95d4
RPINerd <RPINerd@gmail.com>
2023-03-21 15:02:24 -0400

    New rough script to start filtering SAM files

---
+++ cigar_filter.py
@@ -0,0 +1,25 @@
+import argparse
+import sys
+import re
+
+'''
+    cigar_filter.py
+    Chris Howard | 03/21/23
+    
+    For now just a barebones script to filter out reads based on a single CIGAR characteristic. Potential here to develop into something much more full featured.
+'''
+
+
+def main() -> None:
+    
+    for line in open(sys.argv[1]):
+        if line.startswith("@"):
+            continue
+        cols = line.split("\t")
+        # cols[5] is cigar
+        filter = re.match("([0-9]+)M", cols[5])
+        if filter and int(filter[1]) >= 50:
+            print(cols[2], cols[3], cols[5])
+
+if __name__ == "__main__":
+    main()
