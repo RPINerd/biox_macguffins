1b81e96c48ce2ae8b85435715bc89d10ad9eccf8
RPINerd <RPINerd@gmail.com>
2023-05-04 16:17:24 -0400

    Fleshing out some detail

--- pairwise_consensus.py
+++ pairwise_consensus.py
@@ -3,20 +3,30 @@ from Bio import AlignIO
 from Bio import SeqIO
 from Bio.Align import AlignInfo
 
-save each fasta into a struct
+def main(file):
 
-for each fasta in struct
-    
-    for each other fasta in struct
+    records = []
+    try:
+        for record in SeqIO.parse(file, "fasta"):
+            records.append(record)
+    except Exception as e:
+        print(f"Error on record parsing: {e}")
+        exit
 
-        alignment = AlignIO.read(sys.argv[1], 'fasta')
-        summary_align = AlignInfo.SummaryInfo(alignment)
-        summary_align.dumb_consensus(float(sys.argv[2]))
-        
-        if align better
-            save to best alignment
-        else next fasta
+    for sequence in records:
+        reference_list = records.remove(sequence)
+        for ref_sequence in reference_list:
 
-    save best match to new struct
+            alignment = AlignIO.align()
+            summary_align = AlignInfo.SummaryInfo(alignment)
+            summary_align.dumb_consensus(float(sys.argv[2]))
+            
+            if align better
+                save to best alignment
+            else next fasta
 
-    
+        save best match to new struct
+
+if __name__ == "__main__":
+    input_fasta = sys.argv[1]
+    main(input_fasta)
