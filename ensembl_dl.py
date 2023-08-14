e10554c1afff84f9291d9a4d626a2cb7a339cfc1
RPINerd <RPINerd@gmail.com>
2023-08-14 15:35:13 -0400

    Quick script to pull ensemble seqs

---
+++ ensembl_dl.py
@@ -0,0 +1,17 @@
+import sys
+
+import requests
+
+server = "https://rest.ensembl.org"
+
+with open(sys.argv[1]) as id_list:
+    for line in id_list:
+        enst = line.strip().split(".")[0]
+        ext = f"/sequence/id/{enst}?type=cdna"
+        r = requests.get(server + ext, headers={"Content-Type": "text/x-fasta"})
+
+        if not r.ok:
+            r.raise_for_status()
+            sys.exit()
+
+        print(r.text)
