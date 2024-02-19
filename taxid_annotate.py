ee4dccba2e5e3b94ad2bcf7955e5303cd533d191
RPINerd <RPINerd@gmail.com>
2024-02-19 09:50:45 -0500

    Added a script to annotate taxid info

---
+++ taxid_annotate.py
@@ -0,0 +1,85 @@
+import sys
+
+from Bio import SeqIO
+
+
+def cross_reference(ref_records, component, alternates=[]):
+    # Step through the ref_records and search for a match
+    # Return the best match
+    best_match = None
+    # print(component)
+    all_components = [component] + alternates
+    for component in all_components:
+        for record in ref_records:
+            if component.lower() in record.description.lower().replace(",", ""):
+                # print(component, record.description)
+                if best_match is None:
+                    best_match = record
+                    # print("new best match", best_match.description)
+                elif record.name.startswith("NC_"):
+                    if best_match.name.startswith("NC_"):
+                        if len(record.seq) > len(best_match.seq):
+                            best_match = record
+                            # print("new best match", best_match.description)
+                    else:
+                        best_match = record
+                        # print("new best match", best_match.description)
+                elif not best_match.name.startswith("NC_") and len(record.seq) > len(best_match.seq):
+                    best_match = record
+                    # print("new best match", best_match.description)
+
+    return best_match
+
+
+def main(reference, panel_components):
+    # First parse the reference fasta fna file using biopython
+    ref_records = list(SeqIO.parse(reference, "fasta"))
+
+    # Next step through the panel components file and search for matches in the ref_records
+    # If a match is found, write the record to a new file
+    with open(panel_components, "r") as panel, open("panel_annotations.tsv", "a") as output:
+        for line in panel:
+            line = line.strip().split("\t")
+            component = line[0]
+            try:
+                alternates = line[1].split(",")
+            except IndexError:
+                alternates = []
+
+            # if line.startswith("#"):
+            #     continue
+
+            best_match = cross_reference(ref_records, component, alternates)
+
+            if best_match is not None:
+                ambig = str(best_match.seq).count("N")
+                output.write(
+                    f"{best_match.id}\t{component}\t{best_match.description}\t{len(best_match.seq)}\t{ambig}\n"
+                )
+            else:
+                # for alternate in alternates:
+                #     best_match = cross_reference(ref_records, alternate)
+                #     if best_match is not None:
+                #         ambig = str(best_match.seq).count("N")
+                #         output.write(
+                #             f"{best_match.id}\t{component}\t{best_match.description}\t{len(best_match.seq)}\t{ambig}\n"
+                #         )
+                #         break
+                # if best_match is None:
+                output.write("No match found\t" + component + "\n")
+
+
+if __name__ == "__main__":
+    # Get arguments from command line if they're present
+    if len(sys.argv) == 1:
+        reference = "genomic.fna"
+        query = "panel_components.txt"
+    elif len(sys.argv) == 2:
+        reference = sys.argv[1]
+        panel_components = "panel_components.txt"
+    elif len(sys.argv) == 3:
+        reference = sys.argv[1]
+        panel_components = sys.argv[2]
+    else:
+        print("Error: Too many arguments!")
+    main(reference, panel_components)
