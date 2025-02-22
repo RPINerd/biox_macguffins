ed06fbd98cb250ea443cc069f063f575652f2932
RPINerd <RPINerd@gmail.com>
2025-02-21 20:17:11 -0500

    Moved many scripts to a retirement folder

--- tools/blastools.py
+++ tools/blastools.py
@@ -6,22 +6,28 @@
 
 import argparse
 import re
+from pathlib import Path
 
 
-## Remove Self Hits
-# Given a blastn report file, save only the entries which are not self-hits
-def self_ref_remove(in_lines):
-    r_str = "^(chr.+:[0-9]+\-[0-9]+)\t(chr.+:[0-9]+\-[0-9]+)"
+def self_ref_remove(in_lines: list[str]) -> list[str]:
+    """
+    Remove self hits from a blastn report file
+
+    Args:
+        in_lines (list[str]): List of lines from the blastn report file
+
+    Returns:
+        list[str]: List of lines with self hits removed
+    """
+    r_str = r"^(chr.+:[0-9]+\-[0-9]+)\t(chr.+:[0-9]+\-[0-9]+)"
     out = []
 
     total = len(in_lines)
-    prog = 0
-    for line in in_lines:
+    for prog, line in enumerate(in_lines):
         # Feedback
         current_prog = int((prog / total) * 100)
         if current_prog % 5 == 0:
             print("Removing self hits.. " + str(current_prog) + "%", end="\r")
-        prog += 1
 
         hit = re.match(r_str, line)
         if hit.group(1) == hit.group(2):
@@ -33,25 +39,41 @@ def self_ref_remove(in_lines):
     return out
 
 
-def pick_best(in_lines):
+def pick_best(in_lines: list[str]) -> list[str]:
     out = []
 
     return out
 
 
-def main(args):
+def write_output(out_lines: list[str], out_file: str) -> None:
+    """
+    Given the output lines and file, write the output
+
+    Args:
+        out_lines (list[str]): List of lines to write to the output file
+        out_file (str): Path to the output file
+
+    Returns:
+        None
+    """
+    with Path.open(out_file, "w") as out:
+        for line in out_lines:
+            out.write(str(line))
+
+
+def main(args: argparse.Namespace) -> None:
+    """"""
     data = []
-    for line in open(args.input):
-        data.append(line)
+    with Path.open(args.input) as file:
+        for line in file:
+            data.append(line)
 
     if args.trimselfhits:
         data = self_ref_remove(data)
     if args.pickbesthit:
         data = pick_best(data)
 
-    out = open(args.output, "w")
-    for line in data:
-        out.write(str(line))
+    write_output(data, args.output)
 
 
 if __name__ == "__main__":
