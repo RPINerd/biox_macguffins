c036d3fcf6bb768053f37819a3cbbe37d0ea942a
RPINerd <RPINerd@gmail.com>
2026-03-27 11:16:46 -0400

    Rename cache folder for better clarity

--- macguffins/configs.py
+++ macguffins/configs.py
@@ -6,7 +6,7 @@ from Bio import Entrez
 
 Entrez.email = "rpimule@gmail.com"
 
-REFSEQ_CACHE_DIR = Path(__file__).parent.parent / "refseqs"
+REFSEQ_CACHE_DIR = Path(__file__).parent.parent / "cache"
 
 TABIX_PATH = "/home/user/tabix/"
 MIN_LENGTH_COMPLEXITY = 50
